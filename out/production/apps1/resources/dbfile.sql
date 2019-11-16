be5dba333af9bf5bea1e8be19a663fbefe5e0a56
Olzumzum <program.olga@gmail.com>
2019-11-16 18:21:55 +0700

    пофиксила странички (не факт)

--- out/production/apps1/resources/dbfile.sql
+++ out/production/apps1/resources/dbfile.sql
@@ -3,5 +3,20 @@ create table Users (
     user_id int auto_increment primary key,
     email_user varchar(255) not null,
     password_user varchar(255) not null
-)
+);
 
+/** создать таблицу продукции*/
+create table Product (
+    product_id int auto_increment primary key,
+    product_name varchar(255) not null
+);
+
+/*создать таблицу тортов */
+create table Cake_Product(
+    cake_id int auto_increment primary key,
+    product_id int not null,
+    product_name varchar(255) not null,
+    description_product varchar(255),
+    weight int not null,
+    price int not null
+)
