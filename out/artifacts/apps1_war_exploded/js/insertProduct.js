4d71bea54734bf305689b463484309e31007dd3a
Olzumzum <program.olga@gmail.com>
2019-12-05 23:58:50 +0700

    добавила вывод диалогов@

--- out/artifacts/apps1_war_exploded/js/insertProduct.js
+++ out/artifacts/apps1_war_exploded/js/insertProduct.js
@@ -1,15 +1,26 @@
+/**
+ * проверка вводимых данных при добавлении продукции
+ * @param form
+ */
 function verificationEnteredData(form) {
 
     var weightProduct = parseFloat(form.productWeight.value);
     var priceProduct = parseFloat(form.productPrice.value);
 
-    if(typeof weightProduct == "number" && !isNaN(weightProduct))
+    /** проверка на тип вводимых данных
+     * если у полей стоимость и цена тип - не числовой, бд будет плакать (наверное)
+     */
+    if (typeof weightProduct == "number" && !isNaN(weightProduct))
         console.log("Тип поля 'вес' - цифра - все верно");
-    else
+    else {
         console.log("Ошибка ввода поля 'вес' ");
+        $("#succsess_dialog").dialog("open");
+    }
 
-    if(typeof priceProduct == "number" && !isNaN(priceProduct))
+    if (typeof priceProduct == "number" && !isNaN(priceProduct))
         console.log("Тип поля 'цена' - цифра - все верно");
-    else
-        console.log("Ошибка ввода поля 'цена'")
+    else {
+        console.log("Ошибка ввода поля 'цена'");
+        $("#succsess_dialog").dialog("open");
+    }
 }
--- web/js/insertProduct.js
+++ web/js/insertProduct.js
@@ -10,13 +10,17 @@ function verificationEnteredData(form) {
     /** проверка на тип вводимых данных
      * если у полей стоимость и цена тип - не числовой, бд будет плакать (наверное)
      */
-    if(typeof weightProduct == "number" && !isNaN(weightProduct))
+    if (typeof weightProduct == "number" && !isNaN(weightProduct))
         console.log("Тип поля 'вес' - цифра - все верно");
-    else
+    else {
         console.log("Ошибка ввода поля 'вес' ");
+        $("#succsess_dialog").dialog("open");
+    }
 
-    if(typeof priceProduct == "number" && !isNaN(priceProduct))
+    if (typeof priceProduct == "number" && !isNaN(priceProduct))
         console.log("Тип поля 'цена' - цифра - все верно");
-    else
-        console.log("Ошибка ввода поля 'цена'")
+    else {
+        console.log("Ошибка ввода поля 'цена'");
+        $("#succsess_dialog").dialog("open");
+    }
 }
